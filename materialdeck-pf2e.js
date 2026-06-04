8f055f9c6b643f6d20d40ad4757163541fe1248b
Material Foundry <github@materialfoundry.nl>
2026-06-04 16:19:35 +0200

    added documentation link to readme

--- materialdeck-pf2e.js
+++ materialdeck-pf2e.js
@@ -3,7 +3,7 @@ import { otherAction } from "./actions/other.js";
 import { tokenAction } from "./actions/token.js";
 import { Helpers } from "./helpers.js";
 
-export const documentation = "https://materialfoundry.github.io/MaterialDeck_PF2E/";
+export const documentation = "https://materialfoundry.github.io/MaterialDeck_PF2e/";
 
 Hooks.once('MaterialDeck_Ready', () => {
     Helpers.rollModifier = new materialDeck.Helpers.ModeSwitcher('normal', 'mdUpdateRollModifier');
